335b76a9dd5e57d41af0ef82aa3031730e33f9c2
imbishwajit03 <bishwajitgorai195@gmail.com>
2024-12-16 15:55:39 +0530

    User Registration Done

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,10 @@ app.use(cors());
 const connectToDb = require('./db/db');
 connectToDb();
 
+const userRoutes=require('./routes/user.routes')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 
 
 app.get('/', (req, res) => {
@@ -17,6 +21,6 @@ app.get('/', (req, res) => {
 
 })
 
-
+app.use('/users',userRoutes)
 
 module.exports = app;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 const userSchema = new mongoose.Schema({
     fullname: {
-        type: String,
-        required: true,
-        minlenght: [3, 'first name must be at least 3 characters long'],
+        firstname: {
+            type: String,
+            required: true,
+            minlenght: [3, 'first name must be at least 3 characters long'],
+        },
+        lastname: {
+            type: String,
+            minlenght: [3, 'last name must be at least 3 characters long'],
+        }
     },
-    lastname: {
-        type: String,
-        minlenght: [3, 'last name must be at least 3 characters long'],
-    },
+
     email: {
         type: String,
         required: true,
@@ -20,9 +26,25 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         require: true,
+        select: false,
     },
     socketId: {
         type: String,
     }
 })
 
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET)
+    return token;
+}
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
